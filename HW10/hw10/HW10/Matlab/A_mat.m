2c1a258678e34227960f10ad52d37f0f9bb27c0f
Sagar Sachdev <10942307+sagars2@users.noreply.github.com>
2022-01-04 14:18:16 -0800

    Delete HW10/hw10 directory

--- HW10/hw10/HW10/Matlab/A_mat.m
+++
@@ -1,8 +0,0 @@
-function[A0,A1,A2,A3,A13] = A_mat(x) 
-%velocity constraints
-A0 = [];
-A1 = [0,1]; 
-A2 = [1,1];
-A3 = [2*x(1)-4, 2*x(2)-2];
-A13 = [A1;A3];
-end
--- HW10/hw10/HW10/Matlab/dA_mat.asv
+++
@@ -1,7 +0,0 @@
-function[dA0,dA1,dA2,dA3,dA13] = dA_mat(x)
-dA0 = [];
-dA1 = [0,0];
-dA2 = [0,0];
-dA3 = [2*x(3),2*x(4)];
-dA13 = [dA1;dA3];
-end
